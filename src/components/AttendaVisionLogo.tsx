eb9efd151896b0bc9c7cebfdba1bc14cddc862dd
Thapasvi <thapasviraghu@gmail.com>
2025-07-09 16:52:14 +0000

    uhhhh where is the things ive asked foe and the logo not to the mark whe

--- src/components/AttendaVisionLogo.tsx
+++ src/components/AttendaVisionLogo.tsx
@@ -13,9 +13,9 @@ export default function KampusAttendLogo(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <title>KampusAttend Logo</title>
-      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-      <circle cx="9" cy="7" r="4" />
-      <polyline points="16 11 18 13 22 9" />
+      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+      <polyline points="14 2 14 8 20 8" />
+      <path d="m9 15 2 2 4-4" />
     </svg>
   );
 }
